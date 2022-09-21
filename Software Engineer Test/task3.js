4eb421f9f7df4e45e383cc41fbafe5c7bea8e3ed
ayiis <76609039+ayiis218@users.noreply.github.com>
2022-09-21 22:04:32 +0700

    add task

--- Software Engineer Test/task3.js
+++ Software Engineer Test/task3.js
@@ -1,3 +1,15 @@
-const data = [9, 6, 4, 2, 3, 5, 7, 0, 1];
+const array = [9, 6, 4, 2, 3, 5, 7, 0, 1];
 
-function find() {}
+function find(data) {
+   const missArr = [];
+   const length = Math.max(...data);
+   const start = data.indexOf(0) > -1 ? 0 : 1;
+   for (i = start; i < length; i++) {
+      if (data.indexOf(i) < 0) {
+         missArr.push(i);
+      }
+   }
+   return parseInt(missArr);
+}
+
+console.log(find(array));
--- Software Engineer Test/task4.js
+++ Software Engineer Test/task4.js
@@ -1,3 +1,12 @@
-const data = ['flower', 'flow', 'flight'];
+const array = ['flower', 'flow', 'flight'];
 
-function find() {}
+function prefix(data) {
+   if (!data[0] || data.length == 1) return data[0] || '';
+   const i = 0;
+
+   while (data[0][i] && data.every((w) => w[i] === data[0][i])) i++;
+
+   return data[0].substr(0, i).toLowerCase();
+}
+
+console.log(prefix(array));
